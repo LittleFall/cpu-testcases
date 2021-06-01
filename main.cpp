e0ba14878c0f2507cae0de91e6d7b1b29ee9f430
Zhi Qi <qizhi@pingcap.com>
2021-06-01 17:44:02 +0800

    multi-thread

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <thread>
+#include <vector>
+
+void run(long long cycle) {
+    while(cycle--) {
+
+    }
+}
+
+int main() {
+    long long cycle = 1e9;
+    long long core = 48;
+
+    std::vector<std::thread> save;
+
+    for(int i=0; i<core; ++i)
+        save.emplace_back(run, cycle);
+
+    for(auto &th:save)
+        th.join();
+}
--- single-thread.cpp
+++
@@ -1,8 +0,0 @@
-#include<cstdio>
-
-int main(void) {
-	long long cycle = 1e11;
-	while(cycle--) {
-
-	}
-}
