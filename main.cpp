6a208a4ffe170c9f9672edc0e4429453b6c71372
Zhi Qi <qizhi@pingcap.com>
2021-06-02 16:58:38 +0800

    24 tasks per second.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <chrono>
 
 char str[100'000'000];
 
@@ -18,19 +20,30 @@ void write(long long cycle, int id) {
 }
 
 int main() {
+    using std::chrono::system_clock;
+    std::time_t tt;
 
-    for (int i=0; i<100'000'000; ++i) {
-        str[i] = 'c';
-    }
-
-
-    long long cycle = 5e12;
-    long long core = 1000;
+    long long cycle = 1e10;
+    long long core = 24;
 
     std::vector<std::thread> save;
 
-    for(int i=0; i<core; ++i)
-        save.emplace_back(write, cycle, i);
+    constexpr std::chrono::duration<int> s1(1);
+    for (int count = 0; count < 10000; count++)
+    {
+        std::this_thread::sleep_for(s1); // time waster for test
+
+        tt = std::chrono::system_clock::to_time_t ( std::chrono::system_clock::now() );
+        std::cout << "now is: " << ctime(&tt) << ", creating " << core << " task.\n";
+
+        for(int i=0; i<core; ++i)
+            save.emplace_back(run, cycle, i);
+    }
+
+
+
+//    for(int i=0; i<core; ++i)
+//        save.emplace_back(write, cycle, i);
 //    for(int i=core/2; i<core; ++i)
 //        save.emplace_back(write, cycle, i);
 
