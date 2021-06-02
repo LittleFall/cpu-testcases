7d9d4f76ef232ae01206cd9e7d529adf5fff205a
Zhi Qi <qizhi@pingcap.com>
2021-06-02 14:40:03 +0800

    24 write, 24 empty

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <thread>
 #include <vector>
+#include <fstream>
+#include <ostream>
 
-void run(long long cycle) {
+void run(long long cycle, int id) {
     while(cycle--) {
+    }
+}
 
+void write(long long cycle, int id) {
+    std::ofstream fout("a.out"+std::to_string(id));
+    while(cycle--) {
+        fout << "c" << std::endl;
     }
 }
 
@@ -11,11 +19,12 @@ int main() {
     long long cycle = 5e12;
     long long core = 24;
 
-//    run(cycle);
     std::vector<std::thread> save;
 
-    for(int i=0; i<core; ++i)
-        save.emplace_back(run, cycle);
+    for(int i=0; i<core/2; ++i)
+        save.emplace_back(run, cycle, i);
+    for(int i=core/2; i<core; ++i)
+        save.emplace_back(run, cycle, i);
 
     for(auto &th:save)
         th.join();
