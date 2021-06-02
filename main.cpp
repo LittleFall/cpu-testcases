f4cf689ac6a0382597d182cd851c36ce86ba980a
Zhi Qi <qizhi@pingcap.com>
2021-06-02 20:06:06 +0800

    100*1e7

--- main.cpp
+++ main.cpp
@@ -23,29 +23,28 @@ int main(int argc, char **args) {
     using std::chrono::system_clock;
     std::time_t tt;
 
-    long long cycle = 1e12;
-    long long core = 24;
+    long long cycle = 1e7;
+    long long core = 100;
 
     if (argc==2) {
         core = std::atoi(args[1]);
     }
     std::cout << "threads = " << core << std::endl;
+    std::cout << "cycles = " << cycle << std::endl;
 
     std::vector<std::thread> save;
 
-//    constexpr std::chrono::duration<int> s1(1);
-//    for (int count = 0; count < 10000; count++)
-//    {
-//        std::this_thread::sleep_for(s1); // time waster for test
-//
-//        tt = std::chrono::system_clock::to_time_t ( std::chrono::system_clock::now() );
-//        std::cout << "now is: " << ctime(&tt) << ", creating " << core << " task.\n";
+    constexpr std::chrono::duration<int> s1(1);
+    for (int count = 0; count < 10000; count++)
+    {
+        std::this_thread::sleep_for(s1); // time waster for test
+
+        tt = std::chrono::system_clock::to_time_t ( std::chrono::system_clock::now() );
+        std::cout << "now is: " << ctime(&tt) << ", creating " << core << " task.\n";
 
         for(int i=0; i<core; ++i)
             save.emplace_back(run, cycle, i);
-//    }
-
-
+    }
 
 //    for(int i=0; i<core; ++i)
 //        save.emplace_back(write, cycle, i);
