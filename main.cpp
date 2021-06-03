d5a74eb818a534710768ac59b3d893256e458683
Zhi Qi <qizhi@pingcap.com>
2021-06-03 20:25:04 +0800

    1000 write fix

--- main.cpp
+++ main.cpp
@@ -2,10 +2,8 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
-#include <string>
-#include <chrono>
 
-char str[100'000'000];
+char str[100'000'001];
 
 void run(long long cycle, int id) {
     while(cycle--) {
@@ -19,38 +17,20 @@ void write(long long cycle, int id) {
     }
 }
 
-int main(int argc, char **args) {
-    using std::chrono::system_clock;
-    std::time_t tt;
+int main() {
 
-    long long core = 100;
-    long long cycle = 1e7;
+    for (int i=0; i<100'000'000; ++i) {
+        str[i] = 'c';
+    }
 
-    if (argc==2) {
-        core = std::atoi(args[1]);
-    }
-    if (argc==3) {
-        cycle = std::atoi(args[2]);
-    }
-    std::cout << "threads = " << core << std::endl;
-    std::cout << "cycles = " << cycle << std::endl;
+
+    long long cycle = 5e12;
+    long long core = 1000;
 
     std::vector<std::thread> save;
 
-    constexpr std::chrono::duration<int> s1(1);
-    for (int count = 0; count < 10000; count++)
-    {
-        std::this_thread::sleep_for(s1); // time waster for test
-
-        tt = std::chrono::system_clock::to_time_t ( std::chrono::system_clock::now() );
-        std::cout << "Creating " << core << " tasks at: " << ctime(&tt);
-
-        for(int i=0; i<core; ++i)
-            save.emplace_back(run, cycle, i);
-    }
-
-//    for(int i=0; i<core; ++i)
-//        save.emplace_back(write, cycle, i);
+    for(int i=0; i<core; ++i)
+        save.emplace_back(write, cycle, i);
 //    for(int i=core/2; i<core; ++i)
 //        save.emplace_back(write, cycle, i);
 
